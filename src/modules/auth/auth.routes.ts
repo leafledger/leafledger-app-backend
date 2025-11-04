d6a817d67a4fb70f295c7429b686a1d943197593
waquas-ahmed <waquasahmed05@gmail.com>
2025-11-04 19:40:09 +0530

    Add refresh api feature

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { signup, login } from "./auth.controller";
+import { signup, login, protect, catalog } from "./auth.controller";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+// API for AUTH
+router.post("/auth/signup", signup);
+router.post("/auth/login", login);
+
+// API for product
+router.route("/catalog").get(protect, catalog)
 
 export default router;
---
+++ src/utils/jwt.util.ts
@@ -0,0 +1,28 @@
+import jwt, { Secret } from "jsonwebtoken";
+
+// Create tokens
+const ACCESS_TOKEN_SECRET: Secret = process.env.ACCESS_TOKEN_SECRET || "access_secret_leafledger";
+const REFRESH_TOKEN_SECRET: Secret = process.env.REFRESH_TOKEN_SECRET || "refresh_secret_leafledger";
+
+const ACCESS_EXPIRES_IN = process.env.ACCESS_EXPIRES_IN;
+const REFRESH_EXPIRES_IN = process.env.REFRESH_EXPIRES_IN;
+
+interface UserPayload {
+  email: string;
+  id: string;
+}
+
+export const generateTokens = (user: UserPayload) => {
+
+  const accessToken = jwt.sign(
+    { id: user.id, email: user.email },
+    ACCESS_TOKEN_SECRET,
+    { expiresIn: ACCESS_EXPIRES_IN } as jwt.SignOptions
+  );
+  const refreshToken = jwt.sign(
+    { id: user.id, email: user.email },
+    REFRESH_TOKEN_SECRET,
+    { expiresIn: REFRESH_EXPIRES_IN } as jwt.SignOptions
+  );
+  return { accessToken, refreshToken };
+};
